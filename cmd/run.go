8123431162a71aa97ecba3b7a38e41892061198d
Markus Meyer <hydrantanderwand@gmail.com>
2024-07-28 15:36:07 +0200

    docs: Document commands (#41)

--- cmd/run.go
+++ cmd/run.go
@@ -7,14 +7,54 @@ import (
 	"github.com/wndhydrnt/saturn-bot/pkg/options"
 )
 
+var (
+	runCommandHelp = `Execute tasks against repositories.
+
+"run" executes all tasks in the given task file(s).
+It first lists all repositories from the source, for example GitHub or GitLab.
+Then it modifies a repository by executing the actions of a task if the filters
+of that task match.
+If files have been modified, it creates a pull request for the repository.
+
+Examples:
+
+Execute task in file "task.yaml" against all repositories:
+
+saturn-bot run \
+  --config config.yaml \
+  --task task.yaml
+
+Execute tasks in files "task1.yaml" and "task2.yaml"
+against all repositories:
+
+saturn-bot run \
+  --config config.yaml \
+  --task task1.yaml \
+  --task task2.yaml
+
+Globbing support:
+
+saturn-bot run \
+  --config config.yaml \
+  --task *.yaml
+
+Execute task in file "task.yaml" against
+repository "github.com/wndhydrnt/saturn-bot-example":
+
+saturn-bot run \
+  --config config.yaml \
+  --task task.yaml
+  --repository github.com/wndhydrnt/saturn-bot-example`
+)
+
 func createRunCommand() *cobra.Command {
 	var repositories []string
 	var taskFiles []string
 
 	var cmd = &cobra.Command{
 		Use:   "run",
-		Short: "Run locally",
-		Long:  "Run locally",
+		Short: "Execute tasks against repositories",
+		Long:  runCommandHelp,
 		Run: func(cmd *cobra.Command, args []string) {
 			cfg, err := config.Read(cfgFile)
 			handleError(err, cmd.ErrOrStderr())
@@ -25,7 +65,11 @@ func createRunCommand() *cobra.Command {
 		},
 	}
 	cmd.Flags().StringVar(&cfgFile, "config", "", "Path to config file")
-	cmd.Flags().StringArrayVar(&repositories, "repository", []string{}, "Name of a repository to apply the tasks to. Can be supplied multiple times.")
-	cmd.Flags().StringArrayVar(&taskFiles, "task", []string{}, "Path to a file to read Tasks from. Can be supplied multiple times.")
+	cmd.Flags().StringArrayVar(&repositories, "repository", []string{}, `Name of a repository to apply the tasks to.
+Filters of a task aren't executed if this flag
+is set.
+Can be supplied multiple times.`)
+	cmd.Flags().StringArrayVar(&taskFiles, "task", []string{}, `Path to a file to read Tasks from.
+Can be supplied multiple times.`)
 	return cmd
 }
--- cmd/try.go
+++ cmd/try.go
@@ -7,6 +7,40 @@ import (
 	"github.com/wndhydrnt/saturn-bot/pkg/options"
 )
 
+var (
+	tryCommandHelp = `Try out a task locally.
+
+"try" verifies that all filters and that actions modify files
+in a repository.
+
+It first executes all filters against the given repository and
+provides feedback on whether they match or not.
+If all filters match, it clones the repository, applies all
+actions and provides feedback on whether files have changed or not.
+
+Use this command during local development of a task to try it out
+and iterate frequently.
+
+Examples:
+
+Try all tasks in file "task.yaml" against
+repository "github.com/wndhydrnt/saturn-bot-example".
+
+saturn-bot try \
+  --config config.yaml \
+  --repository github.com/wndhydrnt/saturn-bot-example \
+  --task-file task.yaml
+
+Try task "example" in "task.yaml" against
+repository "github.com/wndhydrnt/saturn-bot-example".
+
+saturn-bot try \
+  --config config.yaml \
+  --repository github.com/wndhydrnt/saturn-bot-example \
+  --task-file task.yaml \
+  --task-name example`
+)
+
 func createTryCommand() *cobra.Command {
 	var dataDir string
 	var repository string
@@ -16,7 +50,7 @@ func createTryCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "try",
 		Short: "Try out a task locally",
-		Long:  "Try out a task locally",
+		Long:  tryCommandHelp,
 		Run: func(cmd *cobra.Command, args []string) {
 			cfg, err := config.Read(cfgFile)
 			handleError(err, cmd.ErrOrStderr())
@@ -31,10 +65,11 @@ func createTryCommand() *cobra.Command {
 			handleError(err, cmd.ErrOrStderr())
 		},
 	}
-	cmd.Flags().StringVar(&cfgFile, "config", "", "path to config file.")
-	cmd.Flags().StringVar(&dataDir, "data-dir", "", "path to directory to clone the repository.")
-	cmd.Flags().StringVar(&repository, "repository", "", "name of the repository to test against.")
-	cmd.Flags().StringVar(&taskFile, "task-file", "", "path to the task file to try out.")
-	cmd.Flags().StringVar(&taskName, "task-name", "", "if set, try only the task that matches the name. useful if a task file contains multiple tasks.")
+	cmd.Flags().StringVar(&cfgFile, "config", "", "Path to config file.")
+	cmd.Flags().StringVar(&dataDir, "data-dir", "", "Path to directory to clone the repository.")
+	cmd.Flags().StringVar(&repository, "repository", "", "Name of the repository to test against.")
+	cmd.Flags().StringVar(&taskFile, "task-file", "", "Path to the task file to try out.")
+	cmd.Flags().StringVar(&taskName, "task-name", "", `If set, try only the task that matches the name.
+Useful if a task file contains multiple tasks.`)
 	return cmd
 }
