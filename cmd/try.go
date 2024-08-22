f36cdcc9b3f8d5695609833bef335118788b285f
Markus Meyer <hydrantanderwand@gmail.com>
2024-08-22 20:23:17 +0200

    feat(command): Align args and flags of command "try" with "run" (#63)

--- cmd/try.go
+++ cmd/try.go
@@ -10,16 +10,16 @@ import (
 var (
 	tryCommandHelp = `Try out a task locally.
 
-"try" verifies that all filters and that actions modify files
+"try" verifies that all filters match and that actions modify files
 in a repository.
+
+Use this command during local development of a task to try it out
+and iterate frequently.
 
 It first executes all filters against the given repository and
 provides feedback on whether they match or not.
 If all filters match, it clones the repository, applies all
 actions and provides feedback on whether files have changed or not.
-
-Use this command during local development of a task to try it out
-and iterate frequently.
 
 Examples:
 
@@ -29,34 +29,34 @@ repository "github.com/wndhydrnt/saturn-bot-example".
 saturn-bot try \
   --config config.yaml \
   --repository github.com/wndhydrnt/saturn-bot-example \
-  --task-file task.yaml
+  task.yaml
 
-Try task "example" in "task.yaml" against
+Try task with name "example" in "task.yaml" against
 repository "github.com/wndhydrnt/saturn-bot-example".
 
 saturn-bot try \
   --config config.yaml \
   --repository github.com/wndhydrnt/saturn-bot-example \
-  --task-file task.yaml \
-  --task-name example`
+  --task-name example \
+  task.yaml`
 )
 
 func createTryCommand() *cobra.Command {
 	var dataDir string
 	var repository string
-	var taskFile string
 	var taskName string
 
 	cmd := &cobra.Command{
-		Use:   "try",
+		Use:   "try FILE",
 		Short: "Try out a task locally",
 		Long:  tryCommandHelp,
+		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			cfg, err := config.Read(cfgFile)
 			handleError(err, cmd.ErrOrStderr())
 			opts, err := options.ToOptions(cfg)
 			handleError(err, cmd.ErrOrStderr())
-			runner, err := command.NewTryRunner(opts, dataDir, repository, taskFile, taskName)
+			runner, err := command.NewTryRunner(opts, dataDir, repository, args[0], taskName)
 			if err != nil {
 				handleError(err, cmd.ErrOrStderr())
 			}
@@ -68,7 +68,6 @@ func createTryCommand() *cobra.Command {
 	cmd.Flags().StringVar(&cfgFile, "config", "", "Path to config file.")
 	cmd.Flags().StringVar(&dataDir, "data-dir", "", "Path to directory to clone the repository.")
 	cmd.Flags().StringVar(&repository, "repository", "", "Name of the repository to test against.")
-	cmd.Flags().StringVar(&taskFile, "task-file", "", "Path to the task file to try out.")
 	cmd.Flags().StringVar(&taskName, "task-name", "", `If set, try only the task that matches the name.
 Useful if a task file contains multiple tasks.`)
 	return cmd
