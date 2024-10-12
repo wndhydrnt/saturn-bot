d80cdcae6125c0858e3d394bc4e9f443052ed231
Markus Meyer <hydrantanderwand@gmail.com>
2024-10-12 11:35:01 +0200

    use QuerySelectorAll

--- pkg/filter/xpath.go
+++ pkg/filter/xpath.go
@@ -13,20 +13,22 @@ import (
 	"github.com/wndhydrnt/saturn-bot/pkg/params"
 )
 
+// XpathFactory creates Xpath filters.
 type XpathFactory struct{}
 
+// Create implements Factory.
 func (f XpathFactory) Create(params params.Params) (Filter, error) {
-	expr, err := params.String("expression", "")
+	exprRaw, err := params.String("expression", "")
 	if err != nil {
 		return nil, err
 	}
 
-	if expr == "" {
+	if exprRaw == "" {
 		return nil, fmt.Errorf("required parameter `expression` not set")
 	}
 
 	// Compile expression here to return early if it is invalid.
-	_, err = xpath.Compile(expr)
+	expr, err := xpath.Compile(exprRaw)
 	if err != nil {
 		return nil, fmt.Errorf("invalid XPath expression: %w", err)
 	}
@@ -43,15 +45,20 @@ func (f XpathFactory) Create(params params.Params) (Filter, error) {
 	return &Xpath{Expr: expr, Path: path}, nil
 }
 
+// Name implements Factory.
 func (f XpathFactory) Name() string {
 	return "xpath"
 }
 
+// Xpath filters repositories by applying an XPath expression to a file in the repository.
 type Xpath struct {
-	Expr string
+	Expr *xpath.Expr
 	Path string
 }
 
+// Do implements Filter.
+// It downloads a XML document from a repository and queries it via an XPath expression.
+// It returns true if the XPath expression returns at least one node.
 func (x *Xpath) Do(ctx context.Context) (bool, error) {
 	repo, ok := ctx.Value(sbcontext.RepositoryKey{}).(FilterRepository)
 	if !ok {
@@ -72,15 +79,11 @@ func (x *Xpath) Do(ctx context.Context) (bool, error) {
 		return false, fmt.Errorf("parse XML document: %w", err)
 	}
 
-	// Note: QueryAll uses an internal cache to prevent parsing the same expression multiple times.
-	result, err := xmlquery.QueryAll(doc, x.Expr)
-	if err != nil {
-		return false, fmt.Errorf("query XML document: %w", err)
-	}
-
-	return len(result) > 0, nil
+	nodes := xmlquery.QuerySelectorAll(doc, x.Expr)
+	return len(nodes) > 0, nil
 }
 
+// String implements Filter.
 func (x *Xpath) String() string {
 	return fmt.Sprintf("xpath(expression=%s,path=%s)", x.Expr, x.Path)
 }
