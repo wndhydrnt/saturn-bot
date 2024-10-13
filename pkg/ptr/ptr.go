7389bcd50d0208f9c84b39d53aace738024e1ce8
Markus Meyer <hydrantanderwand@gmail.com>
2024-10-13 11:00:57 +0200

    add docs

--- pkg/ptr/ptr.go
+++ pkg/ptr/ptr.go
@@ -1,9 +1,12 @@
+// ptr provides functions to convert from/to pointers.
 package ptr
 
+// To converts a value to pointer.
 func To[T any](v T) *T {
 	return &v
 }
 
+// From dereferences a pointer.
 func From[T any](v *T) T {
 	return *v
 }
