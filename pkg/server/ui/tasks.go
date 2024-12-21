b71cdab6a947183fe5ef143ff5b4f646a6a8dc8e
Markus Meyer <hydrantanderwand@gmail.com>
2024-12-21 22:16:22 +0100

    task get file view

--- pkg/server/ui/tasks.go
+++ pkg/server/ui/tasks.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/wndhydrnt/saturn-bot/pkg/server/api/openapi"
 )
 
@@ -22,4 +23,32 @@ func (u *Ui) ListTasks(w http.ResponseWriter, r *http.Request) {
 	var data dataListTasks
 	data.Tasks = taskList.Tasks
 	renderTemplate("task-list.html", data, w)
+}
+
+type dataGetTaskFile struct {
+	Content string
+	Name    string
+}
+
+func (u *Ui) GetTaskFile(w http.ResponseWriter, r *http.Request) {
+	reqOpts := openapi.GetTaskV1RequestObject{
+		Task: chi.URLParam(r, "name"),
+	}
+	resp, err := u.API.GetTaskV1(r.Context(), reqOpts)
+	if err != nil {
+		renderError(fmt.Errorf("get task: %w", err), w)
+		return
+	}
+
+	switch v := resp.(type) {
+	case openapi.GetTaskV1200JSONResponse:
+		data := dataGetTaskFile{Content: v.Content, Name: v.Name}
+		renderTemplate("task-get-file.html", data, w)
+
+	case openapi.GetTaskV1404JSONResponse:
+		renderApiError(openapi.Error(v), w)
+
+	case openapi.GetTaskV1500JSONResponse:
+		renderApiError(openapi.Error(v), w)
+	}
 }
