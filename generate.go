68c7ca7191583515f45cca086f51abc5fa0c52e5
Markus Meyer <hydrantanderwand@gmail.com>
2025-02-16 17:48:12 +0100

    fix cmd generate command

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate sh -c "go run -modfile=./tools/go.mod github.com/bwplotka/mdox fmt --soft-wraps ./docs/commands/*.md"
+//go:generate sh -c "go run -modfile=./tools/go.mod github.com/bwplotka/mdox fmt --soft-wraps ./docs/reference/commands/*.md"
