717596b4e6f55dcb810a5f4b5cb2e0d1bffe2e73
Markus Meyer <hydrantanderwand@gmail.com>
2025-07-20 10:09:27 +0200

    rework; add arch doc

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -300,7 +300,7 @@ func NewContext(ctx context.Context) *protoV1.Context {
 type StdioHandler func(pluginName string, msg []byte)
 
 // NewStdioHandler returns a StdioHandler that adds a message to the log when it receives data.
-// `defaultLevel` denotes the log level at which the data received is logged.
+// `defaultLevel` denotes the level to log messages if a log messages does not set its own level.
 // `stream` is the identifier of the stream from which the data was read.
 func NewStdioHandler(defaultLevel zapcore.Level, stream string) StdioHandler {
 	return func(pluginName string, msg []byte) {
@@ -312,12 +312,11 @@ func NewStdioHandler(defaultLevel zapcore.Level, stream string) StdioHandler {
 
 		var lvl zapcore.Level
 		err := lvl.UnmarshalText(parts[0])
-		if err != nil {
+		if err == nil {
+			log.Log().Logw(lvl, string(parts[1]), "plugin", pluginName, "stream", stream)
+		} else {
 			log.Log().Logw(defaultLevel, string(msg), "plugin", pluginName, "stream", stream)
-			return
 		}
-
-		log.Log().Logw(lvl, string(parts[1]), "plugin", pluginName, "stream", stream)
 	}
 }
 
