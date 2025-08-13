81f81384b1b014d80ffce738203ce73dbc411b6d
Markus Meyer <hydrantanderwand@gmail.com>
2025-08-13 16:48:21 +0200

    use local binary

--- hack/generate-command-docs.sh
+++ hack/generate-command-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm ./docs/reference/commands/*.txt
-find ./docs/reference/commands/ -type f -name "*.md" -print0 | xargs -0 basename -s .md | xargs -I % sh -c 'saturn-bot % --help > ./docs/reference/commands/%.txt'
+find ./docs/reference/commands/ -type f -name "*.md" -print0 | xargs -0 basename -s .md | xargs -I % sh -c './saturn-bot % --help > ./docs/reference/commands/%.txt'
